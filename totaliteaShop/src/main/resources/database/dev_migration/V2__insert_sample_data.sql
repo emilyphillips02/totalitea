3308620355366448292d3f39e9c816a2d0ecb03f
christabellejacques <christabelle.jacques@capgemini.com>
2025-08-12 10:27:45 +0100

    1. Finsihed schema 2. Moved schema into database folder in resources so that flyaway can run migration automatically. 3. Created sample data so we can test endpoints. 4. Created Dev migration with sample data

--- totaliteaShop/src/main/resources/database/dev_migration/V2__insert_sample_data.sql
+++ totaliteaShop/src/main/resources/database/dev_migration/V2__insert_sample_data.sql
@@ -1,32 +1,38 @@
+
 INSERT INTO users (name, email, password_hash, role)
 VALUES
     ('Alice Brown', 'alice@example.com', 'hashed_password1', 'CUSTOMER'),
     ('Bob Smith', 'bob@example.com', 'hashed_password2', 'CUSTOMER'),
     ('Admin User', 'admin@example.com', 'hashed_password3', 'ADMIN');
 
-INSERT INTO products (supplier, product_name, type, weight_grams, price_gbp,caffeine_content_per_g,category)
+
+INSERT INTO products (supplier, name, type, weight_grams, price_gbp, caffeine_content_mg_per_g, category)
 VALUES
-    ('Twinings', 'Earl Grey', 'Earl Grey' 250, 5.50, 2.5, 'TEA'),
+    ('Twinings', 'Earl Grey', 'Earl Grey', 250, 5.50, 2.5, 'TEA'),
     ('Whittard of Chelsea', 'Chamomile', 'Herbal', 200, 4.50, NULL, 'TEA'),
     ('Twinings', 'Darjeeling', 'Darjeeling', 250, 6.00, 2.8, 'TEA');
 
-INSERT INTO products (supplier, product_name, type, weight_grams, price_gbp,caffeine_content_per_g,category)
+
+INSERT INTO products (supplier, name, type, weight_grams, price_gbp, caffeine_content_mg_per_g, category)
 VALUES
     ('Jacobs', 'Espresso Roast', 'Espresso', 500, 8.00, 12.0, 'COFFEE'),
-    ('Lavazza', 'Cappuccino Blend', 'Cappuccino', 500, 9.50,10.0, 'COFFEE'),
+    ('Lavazza', 'Cappuccino Blend', 'Cappuccino', 500, 9.50, 10.0, 'COFFEE'),
     ('Starbucks', 'House Blend', 'Filter', 500, 8.50, 9.5, 'COFFEE');
+
 
 INSERT INTO shipping_rules (min_weight, max_weight, cost, free_shipping_threshold)
 VALUES
-    (0, 5, 1.50,100.00),
+    (0, 5, 1.50, 100.00),
     (6, 10, 10.00, 100.00),
-    (11, NULL , 15.00, 100.00);
+    (11, NULL, 15.00, 100.00);
+
 
 INSERT INTO orders (user_id, total_price, shipping_cost, status)
 VALUES
     (1, 13.50, 1.50, 'PENDING');
 
+
 INSERT INTO order_items (order_id, product_id, quantity, sub_total)
 VALUES
-    (1,1,2,11.00),
-    (1, 2,1,4.50);
+    (1, 1, 2, 11.00),
+    (1, 2, 1, 4.50);
