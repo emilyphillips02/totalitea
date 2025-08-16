ed3a6cd9329999580d779d7567ba160bcd1fab07
christabellejacques <christabelle.jacques@capgemini.com>
2025-08-16 18:59:27 +0100

    - Deleted all database as it was in dev - Added new schema

--- totaliteaShop/src/main/java/com/totaliteaShop/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.totaliteaShop.repository;
-
-import com.totaliteaShop.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // You can add custom query methods here
-}
