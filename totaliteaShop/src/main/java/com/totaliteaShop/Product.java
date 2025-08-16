b1697fb494763a39fbda6aef3b2de38b7b274860
christabellejacques <christabelle.jacques@capgemini.com>
2025-08-16 22:00:59 +0100

    Models implemented

---
+++ totaliteaShop/src/main/java/com/totaliteaShop/Product.java
@@ -0,0 +1,49 @@
+package com.totaliteaShop;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @ColumnDefault("nextval('products_id_seq')")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "supplier", nullable = false, length = 100)
+    private String supplier;
+
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
+
+    @Column(name = "weight_grams", nullable = false, precision = 6, scale = 2)
+    private BigDecimal weightGrams;
+
+    @Column(name = "price_gbp", nullable = false, precision = 10, scale = 2)
+    private BigDecimal priceGbp;
+
+    @Column(name = "caffeine_content_mg_per_g", precision = 5, scale = 2)
+    private BigDecimal caffeineContentMgPerG;
+
+    @Column(name = "category", nullable = false, length = 20)
+    private String category;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+}
---
+++ totaliteaShop/src/main/java/com/totaliteaShop/User.java
@@ -0,0 +1,40 @@
+package com.totaliteaShop;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @ColumnDefault("nextval('users_id_seq')")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false, length = 150)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @ColumnDefault("'CUSTOMER'")
+    @Column(name = "role", length = 20)
+    private String role;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "date_registered")
+    private Instant dateRegistered;
+
+}
