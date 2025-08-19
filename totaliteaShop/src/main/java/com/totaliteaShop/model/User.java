23623957612b18c891c6a4125a19e8c83f62d2c0
christabellejacques <christabelle.jacques@capgemini.com>
2025-08-19 16:02:42 +0100

    Updated the controller and user class

--- totaliteaShop/src/main/java/com/totaliteaShop/model/User.java
+++ totaliteaShop/src/main/java/com/totaliteaShop/model/User.java
@@ -26,6 +26,9 @@ public class User {
     @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
+    @Transient
+    private String password;
+
     @ColumnDefault("'CUSTOMER'")
     @Column(name = "role", length = 20)
     private String role;
