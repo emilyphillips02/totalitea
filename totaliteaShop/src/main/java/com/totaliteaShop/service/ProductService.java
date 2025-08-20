386e521f1806acbc33f743bc111dd8f6250143a9
christabellejacques <christabelle.jacques@capgemini.com>
2025-08-20 14:48:30 +0100

    -Product Service added

---
+++ totaliteaShop/src/main/java/com/totaliteaShop/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.totaliteaShop.service;
+
+import com.totaliteaShop.model.Product;
+import com.totaliteaShop.respository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategory(category);
+    }
+}
+
