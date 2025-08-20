4c73827a38a97febf70aadccebb30308dd8d6cdd
christabellejacques <christabelle.jacques@capgemini.com>
2025-08-20 14:49:13 +0100

    -Product repository added

---
+++ totaliteaShop/src/main/java/com/totaliteaShop/respository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.totaliteaShop.respository;
+
+
+import com.totaliteaShop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(String category); // optional filter
+}
+
